428278ab54270464ba24545d053762232ad8f01a
ull <telecas20@gmail.com>
2020-04-29 01:36:07 +0900

    Update karb_controller.rb

--- app/controllers/karb_controller.rb
+++ app/controllers/karb_controller.rb
@@ -6,9 +6,6 @@ class KarbController < ApplicationController
     @rest = params[:rest].to_i
     @num = params[:huka][:num].to_i
     @active_palse = ((220 - @old - @rest) * @num / 100) + @rest
-    cnt = Cnt.find_by(id:1)
-    cnt.num = cnt.num.to_i + 1
-    cnt.save
     render("karb/top")
   end
 end
