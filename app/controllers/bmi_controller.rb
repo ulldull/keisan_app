5c946d0900ee98cb1e5b93c4492cdddceca89c03
ull <telecas20@gmail.com>
2020-04-29 01:32:17 +0900

    Update bmi_controller.rb

--- app/controllers/bmi_controller.rb
+++ app/controllers/bmi_controller.rb
@@ -2,6 +2,24 @@ class BmiController < ApplicationController
   def top
   end
   def new
+    @sintyo = params[:sintyo].to_i
+    @taiju = params[:taiju].to_i
+    @bmi = (@taiju.to_f * 10000 / (@sintyo.to_f ** 2)).round(1)
+    @risou = (((@sintyo.to_f / 100) ** 2) * 22).round(1)
+    if @bmi < 18.5
+      @comment = "適正より痩せています"
+    elsif @bmi < 25
+      @comment = "普通の体重です"
+    elsif @bmi < 30
+      @comment ="ちょっと太っています"
+    elsif @bmi < 35
+      @comment = "太っています"
+    elsif @bmi < 40
+      @comment = "だいぶ太っています"
+    else
+      @comment = "かなり重いですね"
+    end
+    @mitinori = (@risou - @taiju).round(1)
     render("bmi/top")
   end
 end
